58a785bf65ca5e925420bb1cac80ab6c68b8a80c
Jonathan Alvarez <j@jonalvarezz.com>
2021-04-18 22:04:19 +0200

    Add vartiants

--- components/PlantCollection.tsx
+++ components/PlantCollection.tsx
@@ -1,22 +1,53 @@
 import Link from 'next/link'
-import { Grid } from '@material-ui/core'
+import { Grid, GridProps } from '@material-ui/core'
 import { Typography } from '@ui/Typography'
 import { Button } from '@ui/Button'
 import { Excerpt } from '@components/Excerpt'
 
 type PlantCollectionProps = {
   plants: Plant[]
+  variant?: 'square' | 'vertical'
 }
 
-export function PlantCollection({ plants }: PlantCollectionProps) {
+export function PlantCollection({
+  plants,
+  variant = 'square',
+}: PlantCollectionProps) {
+  let gridItemProps: GridProps = { xs: 6, md: 4 }
+  let Component: (props: Plant) => JSX.Element = PlantEntrySquare
+
+  if (variant === 'vertical') {
+    Component = PlantEntryVertical
+    gridItemProps = {
+      xs: 12,
+      sm: 6,
+    }
+  }
+
   return (
     <Grid container component="ul" spacing={4}>
       {plants.map((plant) => (
-        <Grid key={plant.id} item xs={12} sm={6}>
-          <PlantEntryVertical {...plant} />
+        <Grid key={plant.id} item {...gridItemProps}>
+          <Component {...plant} />
         </Grid>
       ))}
     </Grid>
+  )
+}
+
+export default function PlantEntrySquare({ image, plantName, slug }: Plant) {
+  return (
+    <div>
+      <img src={`${image.url}`} />
+      <div className="p-4">
+        <Typography variant="h2" className="break-words">
+          {plantName}
+        </Typography>
+        <Link href={`/entry/${slug}`} passHref>
+          <Button>Read more</Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,8 @@ export default function Home() {
   return (
     <Layout>
       <main className="pt-16">
-        <PlantCollection plants={plants.slice(0, 2)} />
+        <PlantCollection plants={plants.slice(0, 2)} variant="vertical" />
+        <PlantCollection plants={plants.slice(0, 6)} variant="square" />
       </main>
     </Layout>
   )
