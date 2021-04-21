2cc50253cf2026be22363a2859d6380eb44482ea
Jonathan Alvarez <j@jonalvarezz.com>
2021-04-21 21:06:18 +0200

    Add RichText

---
+++ components/RichText.tsx
@@ -0,0 +1,29 @@
+import { BLOCKS, Document } from '@contentful/rich-text-types'
+import {
+  documentToReactComponents,
+  Options,
+} from '@contentful/rich-text-react-renderer'
+import { Typography } from '@ui/Typography'
+
+type RichTextProps = {
+  richText: RichText
+  className?: string
+}
+
+const options: Options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (_, children) => (
+      <Typography variant="body1" gutterBottom color="textSecondary">
+        {children}
+      </Typography>
+    ),
+  },
+}
+
+export const RichText = ({ richText, className }: RichTextProps) => {
+  return (
+    <div className={className}>
+      {documentToReactComponents((richText as unknown) as Document, options)}
+    </div>
+  )
+}
