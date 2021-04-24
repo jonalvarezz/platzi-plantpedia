4058afeb98c8bd31967964936089665e296f6a3b
Jonathan Alvarez <j@jonalvarezz.com>
2021-04-25 00:21:50 +0200

    Pages: TopStories index redirection

---
+++ pages/top-stories/index.tsx
@@ -0,0 +1,44 @@
+import Error from 'next/error'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { getAuthorList } from '@api'
+
+/**
+ * Redirects to /top-stories/<first-author-in-the-list>
+ */
+
+export const getServerSideProps: GetServerSideProps<{
+  error: number
+}> = async () => {
+  try {
+    const authors = await getAuthorList({ limit: 10 })
+
+    if (authors.length > 0) {
+      const firstAuthor = authors[0].handle
+
+      return {
+        redirect: {
+          destination: `/top-stories/${firstAuthor}`,
+          permanent: false,
+        },
+      }
+    }
+
+    return {
+      props: {
+        error: 412,
+      },
+    }
+  } catch (e) {
+    return {
+      props: {
+        error: 400,
+      },
+    }
+  }
+}
+
+export default function TopStoriesIndex({
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <Error statusCode={error} />
+}
