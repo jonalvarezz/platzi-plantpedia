da63f473c84dd2fa0adb78e69de668a724a824c3
Jonathan Alvarez <j@jonalvarezz.com>
2021-05-06 23:03:53 +0200

    Button: Remove unused wrapper

--- ui/Button.tsx
+++ ui/Button.tsx
@@ -1,13 +1,2 @@
-import MuiButton, {
-  ButtonProps as MuiButtonProps,
-} from '@material-ui/core/Button'
-
-export type ButtonProps = Exclude<MuiButtonProps, 'disableElevation'>
-
-export const Button = ({
-  variant = 'contained',
-  color = 'primary',
-  ...props
-}: ButtonProps) => (
-  <MuiButton {...props} variant={variant} color={color} disableElevation />
-)
+export { default as Button } from '@material-ui/core/Button'
+export type { ButtonProps } from '@material-ui/core/Button'
