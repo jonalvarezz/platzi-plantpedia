7fa81d40596ffc5a7d03dc7e8550cf3ed53be5a2
Jonathan Alvarez <j@jonalvarezz.com>
2021-05-09 13:53:04 +0200

    💅

--- ui/NavBar.tsx
+++ ui/NavBar.tsx
@@ -73,14 +73,14 @@ function PlantpediaNoLoVeniasVenirLogo({ title }: { title: string }) {
           href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${favicon}</text></svg>`}
         ></link>
       </Head>
-      <Typography
-        variant="h4"
-        className="flex-grow"
-        component="h1"
-        onMouseEnter={toggleCraziness}
-        onMouseLeave={toggleCraziness}
-      >
-        <a href="/">{title}</a>
+      <Typography variant="h4" className="flex-grow" component="h1">
+        <a
+          href="/"
+          onMouseEnter={toggleCraziness}
+          onMouseLeave={toggleCraziness}
+        >
+          {title}
+        </a>
       </Typography>
     </>
   )
