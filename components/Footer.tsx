4af7c1e7a0f8f585464ba9eaa1b4072abf6e3899
Jonathan Alvarez <j@jonalvarezz.com>
2021-05-09 16:44:43 +0200

    i18n: Add header & footer translations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
 import clsx from 'clsx'
+import { useTranslation } from 'next-i18next'
 import { Grid } from '@ui/Grid'
 import { Typography } from '@ui/Typography'
 
 export const Footer = ({ className }: { className?: string }) => {
+  const { t } = useTranslation(['common'])
+
   return (
     <footer
       className={clsx(
@@ -25,26 +28,26 @@ export const Footer = ({ className }: { className?: string }) => {
           </Grid>
           <Grid item xs={6} sm={4}>
             <Typography variant="h5" className="mb-4">
-              Pages
+              {t('pages')}
             </Typography>
             <ul className="p0">
               <li className="pb-1">
-                <a href="/getting-started">Getting started</a>
+                <a href="/getting-started">{t('gettingStarted')}</a>
               </li>
               <li className="pb-1">
-                <a href="/search">Search</a>
+                <a href="/search">{t('search')}</a>
               </li>
               <li className="pb-1">
-                <a href="/top-stories">Top stories</a>
+                <a href="/top-stories">{t('topStories')}</a>
               </li>
             </ul>
           </Grid>
           <Grid item xs={6} sm={3}>
             <Typography variant="h5" className="mb-4">
-              About
+              {t('about')}
             </Typography>
             <p>
-              <a href="https://platzi.com/">Platzi's Next.js Course by</a>{' '}
+              <a href="https://platzi.com/">{t('aboutDescription')}</a>{' '}
               <a href="https://twitter.com/jonalvarezz">@jonalvarezz</a>
             </p>
             <div className="mt-3">
@@ -66,7 +69,7 @@ export const Footer = ({ className }: { className?: string }) => {
         </Grid>
         <div className="mt-20 border-t-2 border-gray-600 text-gray-600 pt-6 flex justify-between">
           <p>
-            Images from
+            {t('imagesFrom')}{' '}
             <a target="_blank" href="https://www.pexels.com" title="Pexels">
               Pexels
             </a>
