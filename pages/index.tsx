c36239962fafbf6c903ab44d9e43963c83b3c020
Jonathan Alvarez <j@jonalvarezz.com>
2021-05-31 13:03:20 +0200

    Home: use getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
-import { useEffect, useState } from 'react'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
 import { Layout } from '@components/Layout'
 import { getPlantList } from '@api'
 import { PlantCollection } from '@components/PlantCollection'
 
-export default function Home() {
-  const [plants, setPlants] = useState<Plant[]>([])
+type HomeProps = {
+  plants: Plant[]
+}
 
-  useEffect(() => {
-    getPlantList({ limit: 10 }).then((receivedData) => setPlants(receivedData))
-  })
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const plants = await getPlantList({ limit: 10 })
 
+  return {
+    props: { plants },
+  }
+}
+
+export default function Home({
+  plants,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
       <PlantCollection plants={plants} variant="square" />
