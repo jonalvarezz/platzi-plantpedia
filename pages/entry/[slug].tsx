fe8502c0b3f8c3847ee994612af5735fdb5de68f
Jonathan Alvarez <j@jonalvarezz.com>
2021-06-03 20:56:51 +0200

    Intl: Add locales to dynamic pages

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -1,3 +1,4 @@
+import flatMap from 'lodash/flatMap'
 import { GetStaticProps, InferGetStaticPropsType, GetStaticPaths } from 'next'
 import Link from 'next/link'
 
@@ -21,6 +22,7 @@ type PlantEntryPageProps = {
 export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
   params,
   preview,
+  locale,
 }) => {
   const slug = params?.slug
 
@@ -31,7 +33,7 @@ export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
   }
 
   try {
-    const plant = await getPlant(slug, preview)
+    const plant = await getPlant(slug, preview, locale)
 
     // Sidebar – This could be a single request since we are using GraphQL :)
     const otherEntries = await getPlantList({
@@ -58,18 +60,28 @@ type PathType = {
   params: {
     slug: string
   }
+  locale: string
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  if (locales == undefined) {
+    throw new Error(
+      'Uh, did you forget to configure locales in your Next.js config?'
+    )
+  }
+
   // Match home query.
   // @TODO how do we generate all of our pages if we don't know the number? 🤔
   const plantEntriesToGenerate = await getPlantList({ limit: 10 })
 
-  const paths: PathType[] = plantEntriesToGenerate.map(({ slug }) => ({
-    params: {
-      slug,
-    },
-  }))
+  const paths: PathType[] = flatMap(
+    plantEntriesToGenerate.map(({ slug }) => ({
+      params: {
+        slug,
+      },
+    })),
+    (path) => locales.map((loc) => ({ locale: loc, ...path }))
+  )
 
   return {
     paths,
