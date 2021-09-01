431a7ca4a6a94e9fd81e8911c5e63c9be54484c7
Jonathan Alvarez <j@jonalvarezz.com>
2021-09-01 22:07:17 +0200

    Type update
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ import Document, {
 } from 'next/document'
 
 // Not directly exported by NextJS (next/types). May change across versions.
-import { Enhancer, AppType } from 'next/dist/next-server/lib/utils'
+import type { Enhancer, AppType } from 'next/dist/shared/lib/utils'
 
 import { ServerStyleSheets } from '@ui/ssr'
 
